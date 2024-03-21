dc29ce2f118afd69c6495a1a61caa6c251309385
capomanpc <capomanpc@gmail.com>
2024-03-22 04:17:03 +0900

    Fix bug

--- api/src/main/java/com/example/api/HelloController.java
+++ api/src/main/java/com/example/api/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
